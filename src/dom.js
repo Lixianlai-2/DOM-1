355488babdced12ca10bf4a8d70077a8a4f9aec5
MarkLee <1643565650@qq.com>
2022-01-06 19:15:32 +0800

    DOM库的删与改部分

--- src/dom.js
+++ src/dom.js
@@ -1,7 +1,7 @@
 window.dom = {
-  // ---------------------------------
+  // ------------------------
   // 下面是增
-  // ---------------------------------
+  // ------------------------
 
   create(node) {
     return document.createElement(node);
@@ -35,24 +35,112 @@ window.dom = {
     dom.child(parent, node);
   },
 
-  // ---------------------------------
+  // -------------------------
   // 下面是删
-  // ---------------------------------
+  // -------------------------
 
   remove(childNode) {
-    childNode.remove();
+    childNode.parentNode.removeChild(childNode); // 兼容ie
+    // childNode.remove(); //较新的做法
+    return childNode;
   },
 
-  // ---------------------------------
+  empty(ParentNode) {
+    // 空数组，用来放被删除的后代节点
+    const array = [];
+
+    // 找到父元素当下的第一个子节点
+    let x = ParentNode.firstChild;
+
+    // 当父元素节点的第一个子节点存在的时候（非null时），执行下面的代码
+    while (x) {
+      // 把第一个子节点删除，然后放进array数组中
+      array.push(dom.remove(ParentNode.firstChild));
+
+      // 然后让后面的那个节点充当父节点的firstChild节点
+      x = ParentNode.firstChild;
+    }
+  },
+
+  // ------------------------
   // 下面是改
-  // ---------------------------------
+  // ------------------------
 
- attribute(node,attr) {
-  
- }
+  // 设置属性与读取属性。根据参数的个数，写不同的代码，就是重载
 
+  attribute(node, attrName, attrValue) {
+    if (arguments.length === 3) {
+      node.setAttribute(attrName, attrValue);
+    } else if (arguments.length === 2) {
+      // 这里记得到return
+      return node.getAttribute(attrName);
+    }
+  },
 
-  // ---------------------------------
+  // attribute(node, attrName, attrValue) {
+  //   if (attrValue) {
+  //     node.setAttribute(attrName, attrValue);
+  //   } else {
+  //     // 这里记得到return
+  //     return node.getAttribute(attrName);
+  //   }
+  // },
+
+  // 改写文本内容。
+  text(node, string) {
+    if (node.innnerText) {
+      node.innnerText = string;
+    } else {
+      node.textContent = string;
+    }
+  },
+
+  // 改写HTML内容
+  html(node, string) {
+    node.innerHTML = string;
+  },
+
+  // 用于修改style。注意value里面不能带有;因为这个错误浪费我20分钟
+  style(node, nameOrString, value) {
+    if (arguments.length === 3) {
+      // 注意，这里需要用[]才能表示变量，而直接用.运算符，会将内容变成字符串
+      return (node.style[nameOrString] = value);
+    } else if (arguments.length === 2) {
+      // 注意类型是字符串
+      if (typeof nameOrString === "string") {
+        node.style = nameOrString;
+
+        // instanceof 运算符用于检测构造函数的 prototype 属性是否出现在某个实例对象的原型链上。
+      } else if (nameOrString instanceof Object) {
+        let Obj = nameOrString;
+        for (let key in Obj) {
+          node.style[key] = Obj[key];
+        }
+      }
+    }
+  },
+
+  // 增加class
+  addClass(node, string) {
+    node.classList.add(string);
+  },
+
+  // 删除class
+  removeClass(node, string) {
+    node.classList.remove(string);
+  },
+
+  // 添加事件监听
+  on(node, eventName, fn) {
+    node.addEventListener(eventName, fn);
+  },
+
+  // 删除事件监听
+  off(node, eventName, fn) {
+    node.removeEventListener(eventName, fn);
+  },
+
+  // ------------------------
   // 下面是查
-  // ---------------------------------
+  // ------------------------
 };
--- src/main.js
+++ src/main.js
@@ -7,26 +7,21 @@ let childDiv = document.querySelector(".childDiv");
 
 let body = document.querySelector("body");
 
-div1.style =
-  "border:1px solid blue;width:100px;height:100px;margin-bottom:5px;";
+// div1.classList.add("block");
+// div2.classList.add("block");
+// div3.classList.add("block");
+// div4.classList.add("block");
+// p.classList.add("block");
 
-div2.style = "border:1px solid red;width:100px;height:100px;";
-
-div3.style = "border:1px solid blue;width:100px;height:100px;margin-top:5px";
-
-div4.style = "border:1px solid orange;width:100px;height:100px;margin-top:5px;";
-
-p.style = "background-color:red;height:100px;";
-
-body.appendChild(div1);
-body.appendChild(div2);
-body.appendChild(div3);
-dom.child(body, div4);
+// body.appendChild(div1);
+// body.appendChild(div2);
+// body.appendChild(div3);
+// dom.child(body, div4);
 
 // 如果父节点没有在页面上呢
 // dom.child(p,childDiv)
-dom.newParent(childDiv, p);
-dom.remove(body);
+// dom.newParent(childDiv, p);
+// dom.remove(body);
 
 // dom.remove(div1);
 // dom.remove(div2);
@@ -34,3 +29,44 @@ dom.remove(body);
 // dom.remove(p);
 
 // div2.parentNode.insertBefore(div3, div2.nextSibling);
+
+// 相当于const  childNodes = empty.childNodes
+// const { chiildNodes } = node;
+
+// 这是删除整个empty，包括其后代元素
+// dom.remove(empty);
+
+// 这是仅仅消除其内部的内容，本身还存在
+// dom.empty(empty);
+
+// console.log(empty);
+
+// console.log(empty.firstChild);
+
+let empty = document.querySelector("#empty");
+
+// dom.text(empty, "给它添加文本内容");
+
+// 设置title
+dom.attribute(empty, "title", "设置title");
+
+// 得到它的id
+// console.log(dom.attribute(empty, "id"));
+
+dom.addClass(empty, "whatEver");
+
+// dom.removeClass(e1, "inner-block");
+
+let fn = () => console.log("clicked!");
+
+dom.on(empty, "click", fn);
+
+// 如果要remove掉一个函数，需要有函数名，因为如果分开写函数，虽然长得一样，但实际上是不同地址的函数对象
+dom.off(empty, "click", fn);
+
+// 注意这里面不能有;
+dom.style(e1, "border", "5px solid green");
+
+dom.style(e2, { border: "1px solid red", padding: "10px" });
+
+dom.style(e3, "border:3px solid blue; width:10px");
